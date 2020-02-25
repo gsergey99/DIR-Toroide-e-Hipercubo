e04b07955ecd95dcdbeed8f180b729743eff1d8e
gsergey99 <gsergey99>
2020-02-25 20:46:43 +0100

    Implementacion de vecinos echa. Continuar con el algoritmo especifico

--- Toroide.c
+++ Toroide.c
@@ -2,38 +2,50 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-//#include "mpi.h"
+#include <math.h>
+#include "mpi.h"
 
 #define archivo "datos.dat"
 #define MAX_BUFFER 1000
-#define L 4 
+ 
 
-void leer_fichero();
+
+int leer_fichero();
+void conocer_vecinos(int rank);
+
 
 int main(int argc, char *argv[]){
 
-    int rank, size;
-    int continuar = 1;
-    int num_nodos = L*L;
     
+    
+    int rank, size, i,L;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    L = sqrt(size);
+    int num_nodos = L*L;
+    float numeros[L*L];
+    int num_leidos;
+       
 
     if(rank ==0){
         if(size!=num_nodos){
             fprintf(stderr,"Error, el número de nodos (%d) no coincide con las dimensiones del Toroide (%d)\n",num_nodos,size);
             continuar =0;
+        }else{
+
+                num_leidos = leer_fichero();
+            }            
+        
         }
 
     }
 
-    printf("Hello World\n");
     MPI_Finalize();
     return 0;
 }
 
-void leer_fichero(){
+int leer_fichero(){
     FILE* fichero;
     char *buffer = malloc(MAX_BUFFER*sizeof(char));
     double lista_numeros[56];
@@ -58,5 +70,49 @@ void leer_fichero(){
       token = strtok(NULL,",");
       num_numeros++;
     }
+    return num_numeros;
+
+}
+void conocer_vecinos(int rank){
+    int norte,sur,este,oeste;
+    int fila = rank/L;
+    int columna = rank%L;
+
+    switch (fila) // Saber los vecinos NORTE y SUR
+    {
+    case 0:
+        norte = rank+L;
+        sur = (L*(L-1))+rank;
+        break;
+    
+    case L-1:
+        norte = columna;
+        sur = rank-L;
+        break;
+    
+    default:
+        norte = rank+L;
+        sur = rank-L;
+        break;
+    }
+
+    switch (columna) //Saber los vecinos ESTE y OESTE
+    {
+    case 0:
+        este = rank+1;
+        oeste = rank+(L-1);
+        break;
+    
+    case L-1:
+        este = rank-(L-1);
+        oeste = rank-1;
+        break;
+    default:
+        este = rank+1;
+        oeste = rank-1;
+        break;
+    }
+
+    printf("Los vecinos de %d son NORTE: %d, SUR: %d, ESTE: %d, OESTE: %d\n",rank,norte,sur,este,oeste);
 
 }
